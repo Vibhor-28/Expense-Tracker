bf968dab7e446cf519fa6c11e1b0001acca650e5
Vibhor-28 <vibhoragarwal2802@gmail.com>
2023-09-19 18:36:17 +0530

    commit

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,2 +1,10 @@
 import 'package:uuid/uuid.dart';
 
+class Expense {
+  const Expense(this.id,this.amount,this.title,this.date);
+  
+  final String id;
+  final double amount;
+  final String title;
+  final DateTime date;
+}
