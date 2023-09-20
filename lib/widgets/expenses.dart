d717b3a467e6148045f9c1e8d4b64de26692d932
Vibhor-28 <vibhoragarwal2802@gmail.com>
2023-09-20 20:47:15 +0530

    commit

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense_constructor.dart';
 
@@ -30,9 +31,9 @@ class _ExpensesState extends State<Expenses> {
         category: Category.travel)
   ];
 
-  void _opemAddExpenseOverlay()
-  {
-      showModalBottomSheet(context: context, builder: builder)
+  void _opemAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
   }
 
   @override
