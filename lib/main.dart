48292d7808a84775a7d6e3452a9e3bdee2dfd0ec
Vibhor-28 <vibhoragarwal2802@gmail.com>
2023-10-05 22:09:16 +0530

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var colortheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 47, 147, 62));
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+     MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: colortheme.onError,
+        colorScheme: colortheme , 
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: colortheme.onPrimaryContainer,
+          foregroundColor: colortheme.primaryContainer
+        )
+      ),
+      home:const  Expenses(),
     ),
   );
 }
