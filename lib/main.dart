96ef24c3b29f97af842c72e5637d4384dbf4274b
Vibhor-28 <vibhoragarwal2802@gmail.com>
2023-10-16 20:06:34 +0530

    chart widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var colortheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 47, 147, 62));
+var darkColortheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 5, 99, 125) , brightness: Brightness.dark);
 void main() {
   runApp(
      MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: darkColortheme,
+         cardTheme: CardTheme().copyWith(
+          margin: EdgeInsets.symmetric(horizontal: 16 , vertical: 8),
+          color: darkColortheme.secondaryContainer
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: darkColortheme.primaryContainer,
+            foregroundColor: darkColortheme.onPrimaryContainer
+          )
+        )
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         scaffoldBackgroundColor: colortheme.onError,
@@ -19,10 +34,12 @@ void main() {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: colortheme.primary
+            backgroundColor: colortheme.primaryContainer,
+            foregroundColor: colortheme.onPrimaryContainer
           )
         )
       ),
+      themeMode: ThemeMode.system,
       home:const  Expenses(),
     ),
   );
--- lib/models/expense_constructor.dart
+++ lib/models/expense_constructor.dart
@@ -11,7 +11,7 @@ const CategoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.leisure: Icons.movie,
   Category.travel: Icons.flight_takeoff,
-  Category.work:  Icons.work,
+  Category.work: Icons.work,
 };
 
 class Expense {
@@ -30,5 +30,29 @@ class Expense {
 
   String get formattedDate {
     return date_formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
